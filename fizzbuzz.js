42af248923c16e3c4e7e76e8e31738783e12528b
Laura Miller <laura.miller03@bbc.co.uk>
2021-02-17 14:32:14 +0000

    Attempting to reverse result string

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,13 +6,24 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // n = parseInt(prompt("Please enter a number: "));
+    var results = ['Buzz', 'Fizz', 'Bang', 'Fezz', 'Bong'];
+
+    function reverseWords(str) {
+        var words = [];
+        words = str.match(/\S+/g);
+        var result = "";
+        for (var i = 0; i < words.length; i++) {
+             result += words[i].split('').reverse().join('') + " ";
+        }
+        return result
+    }
 
     for (let i = 1; i <= 100; i++) {
         let result = '';
         if (i % 3 === 0) result += 'Fizz';
         if (i % 5 === 0) result += 'Buzz';
         if (i % 7 === 0) result += 'Bang';
-        if (i % 11 === 0) result = 'Bong';
+        else if (i % 17 === 0) reverseWords(result);
         console.log(result || i);
 
     };
